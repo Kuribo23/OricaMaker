8dc5c4060d8f0945376de3d05eb30d62ad40d2e9
Peace Scularto <keistic@gmail.com>
2012-06-08 22:25:53 +0800

    changed greeting

--- index.php
+++ index.php
@@ -247,7 +247,7 @@ $app_name = idx($app_info, 'name', '');
     </header>
 
     <section id="get-started">
-      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <p>Welcome to <strong>Vanguard CardMaker</strong>, running on <span>heroku</span>!</p>	  
       <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
     </section>
 
