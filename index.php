8f297361792c816ef1b22aa43d72897a47e58aa7
Peace Scularto <keistic@gmail.com>
2012-06-09 13:26:33 +0800

    static image

--- index.php
+++ index.php
@@ -40,35 +40,35 @@ $facebook = new Facebook(array(
 
 $user_id = $facebook->getUser();
 if ($user_id) {
-  try {
-    // Fetch the viewer's basic information
-    $basic = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    // If the call fails we check if we still have a user. The user will be
-    // cleared if the error is because of an invalid accesstoken
-    if (!$facebook->getUser()) {
-      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
-      exit();
-    }
-  }
+	try {
+		// Fetch the viewer's basic information
+		$basic = $facebook->api('/me');
+	} catch (FacebookApiException $e) {
+		// If the call fails we check if we still have a user. The user will be
+		// cleared if the error is because of an invalid accesstoken
+		if (!$facebook->getUser()) {
+			header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+			exit();
+		}
+	}
 
-  // This fetches some things that you like . 'limit=*" only returns * values.
-  // To see the format of the data you are retrieving, use the "Graph API
-  // Explorer" which is at https://developers.facebook.com/tools/explorer/
-  $likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
+	// This fetches some things that you like . 'limit=*" only returns * values.
+	// To see the format of the data you are retrieving, use the "Graph API
+	// Explorer" which is at https://developers.facebook.com/tools/explorer/
+	$likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
 
-  // This fetches 4 of your friends.
-  $friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
+	// This fetches 4 of your friends.
+	$friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
 
-  // And this returns 16 of your photos.
-  $photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
+	// And this returns 16 of your photos.
+	$photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
 
-  // Here is an example of a FQL call that fetches all of your friends that are
-  // using this app
-  $app_using_friends = $facebook->api(array(
+	// Here is an example of a FQL call that fetches all of your friends that are
+	// using this app
+	$app_using_friends = $facebook->api(array(
     'method' => 'fql.query',
     'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
-  ));
+    ));
 }
 
 // Fetch the basic info of the app that they are using
@@ -79,34 +79,41 @@ $app_name = idx($app_info, 'name', '');
 ?>
 <!DOCTYPE html>
 <html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+<head>
+<meta charset="utf-8" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
 
-    <title><?php echo he($app_name); ?></title>
-    <link rel="stylesheet" href="stylesheets/screen.css" media="Screen" type="text/css" />
-    <link rel="stylesheet" href="stylesheets/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />
+<title><?php echo he($app_name); ?></title>
+<link rel="stylesheet" href="stylesheets/screen.css" media="Screen"
+	type="text/css" />
+<link rel="stylesheet" href="stylesheets/card.css" media="Screen"
+	type="text/css" />
+<link rel="stylesheet" href="stylesheets/mobile.css"
+	media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)"
+	type="text/css" />
 
-    <!--[if IEMobile]>
+<!--[if IEMobile]>
     <link rel="stylesheet" href="mobile.css" media="screen" type="text/css"  />
     <![endif]-->
 
-    <!-- These are Open Graph tags.  They add meta data to your  -->
-    <!-- site that facebook uses when your content is shared     -->
-    <!-- over facebook.  You should fill these tags in with      -->
-    <!-- your data.  To learn more about Open Graph, visit       -->
-    <!-- 'https://developers.facebook.com/docs/opengraph/'       -->
-    <meta property="og:title" content="<?php echo he($app_name); ?>" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="<?php echo AppInfo::getUrl(); ?>" />
-    <meta property="og:image" content="<?php echo AppInfo::getUrl('/logo.png'); ?>" />
-    <meta property="og:site_name" content="<?php echo he($app_name); ?>" />
-    <meta property="og:description" content="My first app" />
-    <meta property="fb:app_id" content="<?php echo AppInfo::appID(); ?>" />
+<!-- These are Open Graph tags.  They add meta data to your  -->
+<!-- site that facebook uses when your content is shared     -->
+<!-- over facebook.  You should fill these tags in with      -->
+<!-- your data.  To learn more about Open Graph, visit       -->
+<!-- 'https://developers.facebook.com/docs/opengraph/'       -->
+<meta property="og:title" content="<?php echo he($app_name); ?>" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="<?php echo AppInfo::getUrl(); ?>" />
+<meta property="og:image"
+	content="<?php echo AppInfo::getUrl('/logo.png'); ?>" />
+<meta property="og:site_name" content="<?php echo he($app_name); ?>" />
+<meta property="og:description" content="My first app" />
+<meta property="fb:app_id" content="<?php echo AppInfo::appID(); ?>" />
 
-    <script type="text/javascript" src="/javascript/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="/javascript/jquery-1.7.1.min.js"></script>
 
-    <script type="text/javascript">
+<script type="text/javascript">
       function logResponse(response) {
         if (console && console.log) {
           console.log('The response was', response);
@@ -162,17 +169,17 @@ $app_name = idx($app_info, 'name', '');
       });
     </script>
 
-    <!--[if IE]>
+<!--[if IE]>
       <script type="text/javascript">
         var tags = ['header', 'section'];
         while(tags.length)
           document.createElement(tags.pop());
       </script>
     <![endif]-->
-  </head>
-  <body>
-    <div id="fb-root"></div>
-    <script type="text/javascript">
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script type="text/javascript">
       window.fbAsyncInit = function() {
         FB.init({
           appId      : '<?php echo AppInfo::appID(); ?>', // App ID
@@ -206,177 +213,188 @@ $app_name = idx($app_info, 'name', '');
       }(document, 'script', 'facebook-jssdk'));
     </script>
 
-    <header class="clearfix">
-      <?php if (isset($basic)) { ?>
-      <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
+	<header class="clearfix">
+	<?php if (isset($basic)) { ?>
+		<p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
 
-      <div>
-        <h1>Welcome to the world!, <strong><?php echo he(idx($basic, 'name')); ?></strong></h1>
-        <p class="tagline">
-          This is your app
-          <a href="<?php echo he(idx($app_info, 'link'));?>" target="_top"><?php echo he($app_name); ?></a>
-        </p>
+		<div>
+			<h1>
+				Welcome to the world!, <strong><?php echo he(idx($basic, 'name')); ?>
+				</strong>
+			</h1>
+			<p class="tagline">
+				This is your app <a href="<?php echo he(idx($app_info, 'link'));?>"
+					target="_top"><?php echo he($app_name); ?> </a>
+			</p>
 
-        <div id="share-app">
-          <p>Share your app:</p>
-          <ul>
-            <li>
-              <a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
-                <span class="plus">Post to Wall</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
-                <span class="speech-bubble">Send Message</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="facebook-button apprequests" id="sendRequest" data-message="Test this awesome app">
-                <span class="apprequests">Send Requests</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <?php } else { ?>
-      <div>
-        <h1>Welcome</h1>
-        <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
-      </div>
-      <?php } ?>
-    </header>
+			<div id="share-app">
+				<p>Share your app:</p>
+				<ul>
+					<li><a href="#" class="facebook-button" id="postToWall"
+						data-url="<?php echo AppInfo::getUrl(); ?>"> <span class="plus">Post
+								to Wall</span> </a>
+					</li>
+					<li><a href="#" class="facebook-button speech-bubble"
+						id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>"> <span
+							class="speech-bubble">Send Message</span> </a>
+					</li>
+					<li><a href="#" class="facebook-button apprequests"
+						id="sendRequest" data-message="Test this awesome app"> <span
+							class="apprequests">Send Requests</span> </a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<?php } else { ?>
+		<div>
+			<h1>Welcome</h1>
+			<div class="fb-login-button" data-scope="user_likes,user_photos"></div>
+		</div>
+		<?php } ?>
+	</header>
 
-    <section id="get-started">
-      <p>Welcome to <strong>Vanguard CardMaker</strong>, running on <span>heroku</span>!</p>	  
-      <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
-    </section>
+	<section id="get-started">
+		<p>
+			Welcome to <strong>Vanguard CardMaker</strong>, running on <span>heroku</span>!
+		</p>
+		<a href="https://devcenter.heroku.com/articles/facebook" target="_top"
+			class="button">Learn How to Edit This App</a>
+	</section>
 
-    <?php
-      if ($user_id) {
-    ?>
+	<?php
+	if ($user_id) {
+		?>
 
-    <section id="samples" class="clearfix">
-      <h1>Examples of the Facebook Graph API</h1>
+	<section id="samples" class="clearfix">
+		<h1>Examples of the Facebook Graph API</h1>
 
-      <div class="list">
-        <h3>A few of your friends</h3>
-        <ul class="friends">
-          <?php
-            foreach ($friends as $friend) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($friend, 'id');
-              $name = idx($friend, 'name');
-          ?>
-          <li>
-            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-              <?php echo he($name); ?>
-            </a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
+		<div class="list">
+			<h3>A few of your friends</h3>
+			<ul class="friends">
+			<?php
+			foreach ($friends as $friend) {
+				// Extract the pieces of info we need from the requests above
+				$id = idx($friend, 'id');
+				$name = idx($friend, 'name');
+				?>
+				<li><a href="https://www.facebook.com/<?php echo he($id); ?>"
+					target="_top"> <img
+						src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square"
+						alt="<?php echo he($name); ?>"> <?php echo he($name); ?> </a>
+				</li>
+				<?php
+			}
+			?>
+			</ul>
+		</div>
 
-      <div class="list inline">
-        <h3>Recent photos</h3>
-        <ul class="photos">
-          <?php
-            $i = 0;
-            foreach ($photos as $photo) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($photo, 'id');
-              $picture = idx($photo, 'picture');
-              $link = idx($photo, 'link');
+		<div class="list inline">
+			<h3>Recent photos</h3>
+			<ul class="photos">
+			<?php
+			$i = 0;
+			foreach ($photos as $photo) {
+				// Extract the pieces of info we need from the requests above
+				$id = idx($photo, 'id');
+				$picture = idx($photo, 'picture');
+				$link = idx($photo, 'link');
 
-              $class = ($i++ % 4 === 0) ? 'first-column' : '';
-          ?>
-          <li style="background-image: url(<?php echo he($picture); ?>);" class="<?php echo $class; ?>">
-            <a href="<?php echo he($link); ?>" target="_top"></a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
+				$class = ($i++ % 4 === 0) ? 'first-column' : '';
+				?>
+				<li style="background-image: url(<?php echo he($picture); ?>);" class="<?php echo $class; ?>">
+					<a href="<?php echo he($link); ?>" target="_top"></a>
+				</li>
+				<?php
+			}
+			?>
+			</ul>
+		</div>
 
-      <div class="list">
-        <h3>Things you like</h3>
-        <ul class="things">
-          <?php
-            foreach ($likes as $like) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($like, 'id');
-              $item = idx($like, 'name');
+		<div class="list">
+			<h3>Things you like</h3>
+			<ul class="things">
+			<?php
+			foreach ($likes as $like) {
+				// Extract the pieces of info we need from the requests above
+				$id = idx($like, 'id');
+				$item = idx($like, 'name');
 
-              // This display's the object that the user liked as a link to
-              // that object's page.
-          ?>
-          <li>
-            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($item); ?>">
-              <?php echo he($item); ?>
-            </a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
+				// This display's the object that the user liked as a link to
+				// that object's page.
+				?>
+				<li><a href="https://www.facebook.com/<?php echo he($id); ?>"
+					target="_top"> <img
+						src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square"
+						alt="<?php echo he($item); ?>"> <?php echo he($item); ?> </a>
+				</li>
+				<?php
+			}
+			?>
+			</ul>
+		</div>
 
-      <div class="list">
-        <h3>Friends using this app</h3>
-        <ul class="friends">
-          <?php
-            foreach ($app_using_friends as $auf) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($auf, 'uid');
-              $name = idx($auf, 'name');
-          ?>
-          <li>
-            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-              <?php echo he($name); ?>
-            </a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
-    </section>
+		<div class="list">
+			<h3>Friends using this app</h3>
+			<ul class="friends">
+			<?php
+			foreach ($app_using_friends as $auf) {
+				// Extract the pieces of info we need from the requests above
+				$id = idx($auf, 'uid');
+				$name = idx($auf, 'name');
+				?>
+				<li><a href="https://www.facebook.com/<?php echo he($id); ?>"
+					target="_top"> <img
+						src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square"
+						alt="<?php echo he($name); ?>"> <?php echo he($name); ?> </a>
+				</li>
+				<?php
+			}
+			?>
+			</ul>
+		</div>
+	</section>
 
-    <?php
-      }
-    ?>
+	<?php
+	}
+	?>
 
-    <section id="guides" class="clearfix">
-      <h1>Learn More About Heroku &amp; Facebook Apps</h1>
-      <ul>
-        <li>
-          <a href="https://www.heroku.com/?utm_source=facebook&utm_medium=app&utm_campaign=fb_integration" target="_top" class="icon heroku">Heroku</a>
-          <p>Learn more about <a href="https://www.heroku.com/?utm_source=facebook&utm_medium=app&utm_campaign=fb_integration" target="_top">Heroku</a>, or read developer docs in the Heroku <a href="https://devcenter.heroku.com/" target="_top">Dev Center</a>.</p>
-        </li>
-        <li>
-          <a href="https://developers.facebook.com/docs/guides/web/" target="_top" class="icon websites">Websites</a>
-          <p>
-            Drive growth and engagement on your site with
-            Facebook Login and Social Plugins.
-          </p>
-        </li>
-        <li>
-          <a href="https://developers.facebook.com/docs/guides/mobile/" target="_top" class="icon mobile-apps">Mobile Apps</a>
-          <p>
-            Integrate with our core experience by building apps
-            that operate within Facebook.
-          </p>
-        </li>
-        <li>
-          <a href="https://developers.facebook.com/docs/guides/canvas/" target="_top" class="icon apps-on-facebook">Apps on Facebook</a>
-          <p>Let users find and connect to their friends in mobile apps and games.</p>
-        </li>
-      </ul>
-    </section>
-  </body>
+	<section class="card">
+		<div class="card-image">
+			<div class="card-template">
+				<section class="card-sect-top">
+					<img class="card-grade" src="images/card/grade/3.png" /> <img
+						class="card-trigger" src="images/card/trigger/critical.png" />
+				</section>
+				<section class="card-sect-body">
+					<img class="card-shield" src="images/card/shield/10000.png" />
+				</section>
+				<section class="card-sect-comment">
+					<span class="card-comment">Can you approve my IT tender?</span>
+				</section>
+				<section class="card-sect-ability">
+					<p class="card-ability">Auto : When this Unit attacks a Vanguard,
+						during that battle, this Unit gains +3000 power.</p>
+					<p class="card-ability">Auto : [Choose 1 Grade 3 Kagero from your
+						hand and discard it] When this Unit attacks, you may pay the cost.
+						If so, for that Battle, this unit gains +10000 Power.</p>
+				</section>
+				<section class="card-sect-title">
+					<span class="card-title">CANDICE OVERLORD</span>
+				</section>
+				<section class="card-sect-attrs">
+					<div class="card-attrs-left">
+						<span class="card-power">6900</span>
+					</div>
+					<div class="card-attrs-right">
+						<span class="card-clan">Succucos</span> 
+						<span class="card-race">Human</span>
+					</div>
+				</section>
+				<section class="card-sect-footer">
+					<span class="card-artist">Desmond</span>
+				</section>
+			</div>
+		</div>
+	</section>
+</body>
 </html>
